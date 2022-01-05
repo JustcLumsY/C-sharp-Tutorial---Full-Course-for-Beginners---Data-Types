3abdaef3f7568cfb01eaaa2d4300a8f5e48ab1c1
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 11:04:28 +0100

    Add project files.

---
+++ C-sharp Tutorial - Full Course for Beginners - Data Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Full_Course_for_Beginners___Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Get Academy";
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase.IndexOf('c'));
+            Console.WriteLine(phrase[2]);
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Substring(4, 3) );
+            Console.WriteLine(phrase.Substring(4));
+        }
+    }
+}
